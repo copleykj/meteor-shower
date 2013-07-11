07dea217c281bd793b8e4d3ff3271b38393ab1a2
Kelly Copley <copleykj@gmail.com>
2013-07-11 08:54:48 -0400

    Fix css not added to client

--- package.js
+++ package.js
@@ -7,5 +7,5 @@ Package.on_use(function (api, where) {
 	api.use(['jquery', "underscore"], ['client', 'server']);
 
 	api.add_files('js/mesosphere.js', ['client', 'server']);
-
+	api.add_files('css/mesosphere.css', 'client'); 
 });
