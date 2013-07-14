8629d05e78736fc3506a00514de87622940a40a4
Olivier Refalo <orefalo@yahoo.com>
2013-07-14 11:11:29 +0200

    transforms tests

--- package.js
+++ package.js
@@ -5,23 +5,19 @@ Package.describe({
 Package.on_use(function (api, where) {
 
     api.use(['jquery', "underscore"], ['client', 'server']);
-
     api.add_files('js/mesosphere.js', ['client', 'server']);
     api.add_files('css/mesosphere.css', 'client');
+
 });
 
 Package.on_test(function (api) {
-    api.use(['jquery', 'underscore', 'test-helpers', 'tinytest', 'underscore-string']);
 
+    api.use(['jquery', 'underscore', 'test-helpers', 'tinytest', 'underscore-string']);
     api.add_files('js/mesosphere.js', ['client', 'server']);
 
-    api.use('session', 'client');
-    api.add_files('tests/required_tests.js', 'client');
-    api.add_files('tests/format_tests.js', 'client');
-    api.add_files('tests/rules_tests.js', 'client');
+    api.add_files('tests/required_tests.js', ['client', 'server']);
+    api.add_files('tests/format_tests.js',  ['client', 'server']);
+    api.add_files('tests/rules_tests.js',  ['client', 'server']);
+    api.add_files('tests/transforms_tests.js',  ['client', 'server']);
 
-//    api.use('http', 'server');
-//    api.add_files('tests/router_server_tests.js', 'server');
-//
-//    api.add_files('tests/router_common_tests.js', ['client', 'server']);
 });
