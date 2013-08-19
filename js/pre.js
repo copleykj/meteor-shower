f46950e671186edfdd1addf22353d993a21f2502
Kelly Copley <copleykj@gmail.com>
2013-08-19 17:52:03 -0400

    fix underscore-string check

--- js/pre.js
+++ js/pre.js
@@ -1,4 +1,4 @@
-if(typeof _.str === "undefined" && Meteor.isServer){
+if(typeof Package['underscore-string'] === "undefined" && Meteor.isServer){
 	var message = "underscore-string dependency for Mesosphere is unresolved. This is likely due to the fact that you are using Meteor 0.6.5 and"+
 	" underscore-string has not been updated on Atmosphere\n\n Please refer to https://github.com/copleykj/Mesosphere/blob/master/README.md for more details";
 	console.log(message);
