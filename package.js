3d80a7b4aea20d747fe9d8e905c02bbfdcdcaccf
digilord <digilord@me.com>
2014-08-26 18:19:17 -0700

    Updated for 0.9.x

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-    summary: "A form-data validation and transformation package for Meteor"
+    summary: "A form-data validation and transformation package for Meteor",
+  version: "0.1.9",
+  git: "https://github.com/copleykj/Mesosphere.git"
 });
 
 Package.on_use(function (api, where) {
+    api.versionsFrom("METEOR@0.9.0");
 
-    api.use(['templating','jquery', 'underscore', 'underscore-string-latest'], ['client', 'server']);
+    api.use(['templating','jquery', 'underscore', "mrt:underscore-string-latest@2.3.3"], ['client', 'server']);
     api.add_files(['js/pre.js','js/mesosphere.js','js/post.js'], ['client', 'server']);
     api.add_files('css/mesosphere.css', 'client');
     api.export('Mesosphere');
