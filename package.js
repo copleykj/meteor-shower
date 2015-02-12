3d2b49b3ce6d8f83f42de8ec9da66a7981ebc78a
Diego Sampaio <chinello@gmail.com>
2015-02-12 09:09:28 -0200

    updated package.js

--- package.js
+++ package.js
@@ -1,23 +1,26 @@
 Package.describe({
-    summary: "A form-data validation and transformation package for Meteor"
+  summary: "A form-data validation and transformation package for Meteor",
+  version: "0.0.1",
+  git: "https://github.com/Konecty/Mesosphere.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.0");
 
-    api.use(['templating','jquery', 'underscore', 'underscore-string-latest'], ['client', 'server']);
-    api.add_files(['js/pre.js','js/mesosphere.js','js/post.js'], ['client', 'server']);
-    api.add_files('css/mesosphere.css', 'client');
+    api.use(['templating','jquery', 'underscore', "mrt:underscore-string-latest@2.3.3"], ['client', 'server']);
+    api.addFiles(['js/pre.js','js/mesosphere.js','js/post.js'], ['client', 'server']);
+    api.addFiles('css/mesosphere.css', 'client');
     api.export('Mesosphere');
 });
 
 Package.on_test(function (api) {
 
-    api.use(['jquery', 'underscore', 'test-helpers', 'tinytest', 'underscore-string-latest']);
-    api.add_files('js/mesosphere.js', ['client', 'server']);
+    api.use(['jquery', 'underscore', 'test-helpers', 'tinytest', 'mrt:underscore-string-latest@2.3.3']);
+    api.addFiles('js/mesosphere.js', ['client', 'server']);
 
-    api.add_files('tests/required_tests.js', ['client', 'server']);
-    api.add_files('tests/format_tests.js',  ['client', 'server']);
-    api.add_files('tests/rules_tests.js',  ['client', 'server']);
-    api.add_files('tests/transforms_tests.js',  ['client', 'server']);
-    api.add_files('tests/aggregate_tests.js', ['client', 'server']);
+    api.addFiles('tests/required_tests.js', ['client', 'server']);
+    api.addFiles('tests/format_tests.js',  ['client', 'server']);
+    api.addFiles('tests/rules_tests.js',  ['client', 'server']);
+    api.addFiles('tests/transforms_tests.js',  ['client', 'server']);
+    api.addFiles('tests/aggregate_tests.js', ['client', 'server']);
 });
